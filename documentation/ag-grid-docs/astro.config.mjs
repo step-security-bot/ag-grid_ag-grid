32214a5f08e8481fb160887af6b17c29676fc0b0
Mark Durrant <mark.j.durrant@gmail.com>
2025-06-10 10:02:04 +0100

    AG-14969 - Revert allowing html in markdoc

--- documentation/ag-grid-docs/astro.config.mjs
+++ documentation/ag-grid-docs/astro.config.mjs
@@ -188,7 +188,7 @@ export default defineConfig({
     integrations: [
         buildTime(),
         react(),
-        markdoc({ allowHTML: true }),
+        markdoc(),
         sitemap(getSitemapConfig()),
         agHtaccessGen({ include: HTACCESS === 'true' }),
         agRedirectsChecker({
